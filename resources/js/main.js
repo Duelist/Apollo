dab16e437395bbe1a5a80db44611d1e6ec4b97c2
Ian Benedict <ianbenedict@gmail.com>
2013-11-12 10:17:48 -0500

    Abstracted and modified block functions.

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,10 +3,74 @@
 (function () {
   "use strict";
 
-  var makeXsBlock = createBlock('md');
-  var makeMdBlock = createBlock('md');
-  var makeLgBlock = createBlock('md');
-  var makeRow = createDiv('row');
+  var makeXsBlock = createBlock(['md', 'lg']);
+  var makeMdBlock = createBlock(['md', 'lg']);
+  var makeLgBlock = createBlock(['md', 'lg']);
+
+  function createDomElement (element) {
+    // TODO: sanity check goes here
+
+    return function (_class) {
+      // TODO: sanity check goes here
+
+      var class_list = (_.isArray(_class)) ? _class.join(' ') : _class;
+
+      return function () {
+        return $(['<', element, '>'].join(''), { 'class': class_list });
+      }
+    }
+  }
+
+  function makeRow() {
+    var createDiv = createDomElement('div');
+    return createDiv('row');
+  }
+
+  function createBlock (block_view) {
+    // TODO: sanity check goes here
+
+    return function (block_size) {
+      // TODO: sanity check goes here
+
+      var classes,
+          createDiv = createDomElement('div');
+
+      console.log(_.isArray(block_view));
+
+      if (_.isArray(block_view)) {
+        classes = _.map(block_view, function (x) { return ['col', block_view, block_size].join('-'); });
+      } else {
+        classes = [['col', block_view, block_size].join('-')];
+      }
+
+      classes.push('block');
+      classes.push('block-idle');
+
+      return createDiv(classes);
+    }
+  }
+
+  function splitBlock (block) {
+    var sizes = [],
+        class_list = [];
+
+    // TODO
+
+    return _.map(sizes, createBlock);
+  }
+
+  function getBlockView (block) {
+
+    // TODO
+
+
+  }
+
+  function getBlockSize (block) {
+
+    // TODO
+
+  }
 
   $(document).ready(function () {
 
@@ -33,35 +97,4 @@
       $(this).addClass("block-selected");
     });
   });
-
-  function createDiv (_class) {
-    var class_list = (_.isArray(_class)) ? _class.join(' ') : _class;
-
-    return function () {
-      return $('<div>', { 'class': class_list });
-    }
-  }
-
-  function createBlock (block_class) {
-    return function (block_size) {
-      return createDiv(['col-' + block_class + '-' + block_size, 'block', 'block-idle']);
-    }
-  }
-
-  function splitBlock (block) {
-    var sizes = [],
-        class_list = [];
-
-  
-
-    return _.map(sizes, createBlock);
-  }
-
-  function getBlockClass () {
-
-  }
-
-  function getBlockSize () {
-
-  }
 })();
